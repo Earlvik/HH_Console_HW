e94fcd7c9ad35f7d1e11f11c7a6465c6cbbe54d9
v.lopatin <earlviktor@gmail.com>
2014-11-30 21:17:24 +0300

    Finished the last script

--- final.sh
+++ final.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+usage="(basename "$0") [-help] -- Программа для вычисления различных данных из логов.
+Домашнее задание по командной строке *nix для школы разработчиков headhunter.
+Для части функционала требуется timeplot: https://github.com/jkff/timeplot
+
+Использование:
+	final.sh filename -- запуск программы для файла filename
+	final.sh -h -- просмотр данной страницы описания"
+
+ 
+while  getopts ':h' option; do
+case "$option" in
+	h) echo "USAGE: $usage"
+	exit 
+	;;
+	\?)printf "illegal option: -%s\n" "$OPTARG" >&2
+       exit 1
+       ;;
+esac
+done
+
 fileName=$1
 echo "Выберите подзадание:"
 echo "1) Найти общее время успешных обращений к /resume, среднее, 95% и 99% квантиль"
@@ -9,7 +29,7 @@ read command
 
 case $command in
 1)
-lines=`grep -E "12:[0-5][0-9]:[0-5][0-9].*/resume.*\s200\s" $fileName | sort -k 8`
+lines=`grep -E "12:[0-5][0-9]:[0-5][0-9].*/resume.*\s2[0-9]{2}\s" $fileName | sort -k 8`
 SUM=0
 NUM=0
 for line in $lines; do
@@ -56,10 +76,74 @@ echo Медиана: ${times[$index50]}
 3)
 echo "Введите день в формате гггг-ММ-дд"
 read day
-awk "/$day.*resume/"'{print $1 " " $2 " =duration.resume " $8}' $fileName > logs.trace
-awk "/$day.*vacancy/"'{print $1 " " $2 " =duration.vacancy " $8}' $fileName >> logs.trace
-awk "/$day.*user/"'{print $1 " " $2 " =duration.user " $8}' $fileName >> logs.trace
-tplot -if logs.trace -o graph.png -k duration 'within[.] lines' 
+#awk "/$day.*resume/"'{print $1 " " $2 " =duration.resume " $8}' $fileName > logs.trace
+#awk "/$day.*vacancy/"'{print $1 " " $2 " =duration.vacancy " $8}' $fileName >> logs.trace
+#awk "/$day.*user/"'{print $1 " " $2 " =duration.user " $8}' $fileName >> logs.trace
+
+for i in {0..23}; do
+
+#hell of a govnocode. Will try to do better
+ 
+grep -E "$day\s$i:.*/resume" $fileName | sort -k 8 > temp;
+NUM=$(wc -l < temp); 
+
+if [[ $NUM == 0 ]]
+then
+continue
+fi
+
+index95=`echo "$NUM * 0.95 +1" | bc`;
+index95=${index95%.*};
+
+sed -n "${index95}p" temp > quantile
+awk -v hour="$i" '{print $1 " " hour":00:00,000 =quantile.resume " $8}' quantile > logs.trace 
+rm temp
+rm quantile
+
+grep -E "$day\s$i:.*/vacancy" $fileName | sort -k 8 > temp;
+NUM=$(wc -l < temp); 
+
+if [[ $NUM == 0 ]]
+then
+continue
+fi
+
+index95=`echo "$NUM * 0.95 +1" | bc`;
+index95=${index95%.*};
+
+sed -n "${index95}p" temp > quantile
+awk -v hour="$i" '{print $1 " " hour":00:00,000 =quantile.vacancy " $8}' quantile > logs.trace 
+rm temp 
+rm quantile
+
+grep -E "$day\s$i:.*/user" $fileName | sort -k 8 > temp;
+NUM=$(wc -l < temp); 
+
+if [[ $NUM == 0 ]]
+then
+continue
+fi
+
+index95=`echo "$NUM * 0.95 +1" | bc`;
+index95=${index95%.*};
+
+sed -n "${index95}p" temp > quantile
+awk -v hour="$i" '{print $1 " " hour":00:00,000 =quantile.user " $8}' quantile > logs.trace 
+rm temp 
+rm quantile
+
+done
+
+if hash tplot 2>/dev/null; then
+        tplot -if logs.trace -o graph.png -k quantile 'within[.] lines' 
+    else
+       echo "Для построения графика нужна утилита timeplot.
+	Код утилиты, документацию и инструкцию по установке вы можете найти здесь:
+	https://github.com/jkff/timeplot"
+	exit  
+fi
+echo "График был сохранён в файл graph.png"
+
 ;;
 4)
 exit 0
